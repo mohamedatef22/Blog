62976bf0d58bca93f6d095327db1c9286072464d
mohamedatef22 <34000758+mohamedatef22@users.noreply.github.com>
2021-03-14 19:42:32 +0200

    add cors

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const confirmation = require("./routes/confirmation");
 const post = require("./routes/posts");
 const app = express();
 const helmet = require("helmet");
+const cors = require("cors");
 const compression = require("compression");
 
 require("./startup/db")();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", user);
 app.use("/api/confirm", confirmation);
 app.use("/api/posts", post);
-app.use(helmet())
-app.use(compression())
+app.use(helmet());
+app.use(compression());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Connected to port: ${port}`));
